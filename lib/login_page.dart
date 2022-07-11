9edc36f826df603aa93a6b30597d762452e59d86
Raphael Hespanhol <rhespanhol@ciandt.com>
2022-07-11 12:00:22 -0300

    Updated the login image

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -12,72 +12,85 @@ class _LoginPageState extends State<LoginPage> {
   String email = "";
   String password = "";
 
-  Widget _body(){
-    return
+  Widget _body() {
+    return SingleChildScrollView(
+      child: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: 250,
+                height: 100,
+                child: Image.asset('assets/images/logo.png'),
+              ),
+              Container(
+                height: 10,
+              ),
+              TextField(
+                onChanged: (text) {
+                  email = text;
+                },
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              TextField(
+                onChanged: (text) {
+                  password = text;
+                },
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              RaisedButton(
+                onPressed: () {
+                  if (email == 'raphael@gmail.com' && password == '123') {
+                    print("login e senhas corretas");
+                    Navigator.of(context).pushReplacementNamed('/home');
+                  } else {
+                    print("login inválido");
+                  }
+                },
+                child: Text('Entrar'),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: SizedBox(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  width: 250,
-                  height: 100,
-                  child: Image.asset('assets/images/logo.png'),
-                ),
-                Container(
-                  height: 10,
-                ),
-                TextField(
-                  onChanged: (text) {
-                    email = text;
-                  },
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: InputDecoration(
-                    labelText: 'Email',
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-                SizedBox(
-                  height: 15,
-                ),
-                TextField(
-                  onChanged: (text) {
-                    password = text;
-                  },
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    labelText: 'Password',
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                RaisedButton(
-                  onPressed: () {
-                    if (email == 'raphael@gmail.com' && password == '123') {
-                      print("login e senhas corretas");
-                      Navigator.of(context).pushReplacementNamed('/home');
-                    } else {
-                      print("login inválido");
-                    }
-                  },
-                  child: Text('Entrar'),
-                )
-              ],
-            ),
-          ),
+        body: Stack(
+      children: [
+        SizedBox(
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            child: Image.asset(
+              'assets/images/background.jpg',
+              fit: BoxFit.cover,
+            )),
+        Container(
+          color: Colors.black.withOpacity(0.3),
         ),
-      ),
-    );
+        _body(),
+      ],
+    ));
   }
 }
